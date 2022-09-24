51922199338ac2ce4b7d6faaadf2a02f0a2b0623
Gunawan <72968411+Gunawan050500@users.noreply.github.com>
2022-09-24 13:09:29 +0700

    tugas7_js_Gunawan_STTNF_Arip
    ini tugas7 javascript

---
+++ tugas7_js/js/myjs-jquery.js
@@ -0,0 +1,263 @@
+$("document").ready(function () {
+    // Hewan Unggas
+    $("#ayam").click(function () {
+        $("#ayam-img").attr({
+            'src': "img/ayam2.png"
+        });
+        $("#ayam-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#ayam-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/ayam.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#bebek").click(function () {
+        $("#bebek-img").attr({
+            'src': "img/bebek.png"
+        });
+        $("#bebek-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#bebek-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/bebek.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#burung").click(function () {
+        $("#burung-img").attr({
+            'src': "img/burungbeo.png"
+        });
+        $("#burung-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#burung-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/beo.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#angsa").click(function () {
+        $("#angsa-img").attr({
+            'src': "img/angsa.png"
+        });
+        $("#angsa-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#angsa-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/angsa.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#pelikan").click(function () {
+        $("#pelikan-img").attr({
+            'src': "img/pelikan.png"
+        });
+        $("#pelikan-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#pelikan-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/pelikan.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+
+    // Hewan Ternak
+    $("#kerbau").click(function () {
+        $("#kerbau-img").attr({
+            'src': "img/kerbau.png"
+        });
+        $("#kerbau-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#kerbau-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/kerbau.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#sapi").click(function () {
+        $("#sapi-img").attr({
+            'src': "img/sapi.png"
+        });
+        $("#sapi-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#sapi-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/sapi.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#kambing").click(function () {
+        $("#kambing-img").attr({
+            'src': "img/kambing.png"
+        });
+        $("#kambing-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#kambing-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/kambing.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#domba").click(function () {
+        $("#domba-img").attr({
+            'src': "img/domba.png"
+        });
+        $("#domba-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#domba-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/domba.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#babi").click(function () {
+        $("#babi-img").attr({
+            'src': "img/babi2.png"
+        });
+        $("#babi-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#babi-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/babi.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+
+    //Hewan Buas
+    $("#harimau").click(function () {
+        $("#harimau-img").attr({
+            'src': "img/harimau.png"
+        });
+        $("#harimau-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#harimau-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/harimau.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#komodo").click(function () {
+        $("#komodo-img").attr({
+            'src': "img/komodo.png"
+        });
+        $("#komodo-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#komodo-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/komodo.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#singa").click(function () {
+        $("#singa-img").attr({
+            'src': "img/singa.png"
+        });
+        $("#singa-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#singa-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/singa.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#beruang").click(function () {
+        $("#beruang-img").attr({
+            'src': "img/beruang.png"
+        });
+        $("#beruang-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#beruang-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/beruang.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+    $("#serigala").click(function () {
+        $("#serigala-img").attr({
+            'src': "img/serigala.png"
+        });
+        $("#serigala-text").css({
+            'top': "75%",
+            'font-size': "30px",
+            '-webkit-text-stroke': "1px black",
+            'font-weight': "bold"
+        })
+        $("#serigala-subtext").hide();
+        $("<audio></audio>").attr({
+            'src': "audio/serigala.mp3",
+            'volume': 0.2,
+            'autoplay': "autoplay"
+        }).appendTo("body");
+    })
+});
